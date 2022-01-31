5034704b31e19b83760043fae06b0b6fec00864d
hanashiro <felipeaah@gmail.com>
2022-01-31 06:22:11 -0300

    aula-02-04

--- pages/produtos/[categoria].tsx
+++ pages/produtos/[categoria].tsx
@@ -1,7 +1,25 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function Categoria() {
     const router = useRouter();
 
-    return <div>Categoria - {router.query.categoria}</div>;
+    return (
+        <div>
+            Categoria - {router.query.categoria} - {router.query.id}
+            <button onClick={() => router.push('/produtos/ABC')}>ABC</button>
+            <ul>
+                <li>
+                    <Link href="/produtos/cosmeticos?id=123">
+                        <a>Cosméticos</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/produtos/doces">
+                        <a>Doces</a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
