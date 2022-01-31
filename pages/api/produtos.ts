b41ad9ede09719457df145132aa2f3ac1aa2696a
hanashiro <felipeaah@gmail.com>
2022-01-31 07:52:28 -0300

    aula-03-03

--- pages/api/produtos.ts
+++ pages/api/produtos.ts
@@ -3,4 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default function ProdutosApi(req: NextApiRequest, res: NextApiResponse) {
     console.log('MÉTODO: ', req.method);
     console.log('OBJETO: ', req.body);
+
+    res.status(200).json({
+        abc: 123,
+    });
 }
