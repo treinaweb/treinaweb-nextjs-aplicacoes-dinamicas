74a0e90c23ea168991410a7ac08ac3f1cc5ec5a8
hanashiro <felipeaah@gmail.com>
2022-02-03 07:48:41 -0300

    projeto-03-04

---
+++ src/pages/api/posts/[postId].ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import { BlogPost } from '../../../data/@types/BlogPostInterface';
+
+const PostsApis = axios.create({
+    baseURL: 'http://localhost:3002/api/posts',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    switch (req.method) {
+        case 'GET':
+            return handleGet(req, res);
+        default:
+            res.status(405).send({ error: 'Method not allowed' });
+    }
+};
+
+async function handleGet(req: NextApiRequest, res: NextApiResponse) {
+    const post = await PostsApis.get('/', {
+        params: {
+            id: req.query.postId,
+        },
+    });
+
+    if (post.data) {
+        res.status(200).json(post.data);
+    } else {
+        res.status(404).send({
+            error: 'Post not found',
+        });
+    }
+}
