5710d34e75a9f0ed6b2d4c23f4dc84afb23cb6dc
hanashiro <felipeaah@gmail.com>
2022-02-04 06:04:19 -0300

    projeto-04-02

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,26 +1,13 @@
+import { useEffect, useState } from 'react';
 import { BlogPost } from '../../@types/BlogPostInterface';
+import { ApiService } from '../../services/ApiService';
 
 export default function useIndex() {
-    const posts: BlogPost[] = [
-        {
-            id: '2',
-            title: 'Comandos do Git que você precisa conhecer - Parte 1',
-            slug: 'comandos-git',
-            description:
-                'Domine o Git ao conhecer comandos e truques que facilitarão o seu trabalho.',
-            picture:
-                'https://dkrn4sk0rn31v.cloudfront.net/uploads/2019/07/29063506/git-1.png',
-        },
-        {
-            id: '1',
-            title: '10 Truques do NPM - Você conhece todos?',
-            slug: 'truques-npm',
-            description:
-                'Conheça simples truques que você pode fazer com o NPM.',
-            picture:
-                'https://dkrn4sk0rn31v.cloudfront.net/uploads/2019/04/23151507/TRUQUES-NPM.png',
-        },
-    ];
+    const [posts, setPosts] = useState<BlogPost[]>([]);
+
+    useEffect(() => {
+        ApiService.get('posts').then((res) => setPosts(res.data));
+    }, []);
 
     return {
         posts,
