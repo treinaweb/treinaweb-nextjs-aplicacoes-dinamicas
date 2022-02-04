78b32214afd3c48f6963aeb38ee8bad8bc181af2
hanashiro <felipeaah@gmail.com>
2022-02-04 08:01:56 -0300

    projeto-05-02

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,33 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-export default NextAuth({});
+export default NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'TreinaBlog',
+            credentials: {
+                username: {
+                    label: 'Nome de usuário',
+                    type: 'text',
+                    placeholder: 'Digite seu nome de usuário',
+                },
+                password: { label: 'Senha', type: 'password' },
+            },
+            async authorize(credentials, req) {
+                if (
+                    credentials &&
+                    credentials.username === 'admin' &&
+                    credentials.password === 'admin'
+                ) {
+                    return {
+                        id: 1,
+                        name: 'Administrador',
+                        email: '',
+                    };
+                }
+
+                return null;
+            },
+        }),
+    ],
+});
